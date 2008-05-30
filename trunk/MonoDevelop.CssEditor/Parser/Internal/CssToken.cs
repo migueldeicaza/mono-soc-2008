a56453180db9872f7569ad5e6240959b72e72cb2
alouca <alouca@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-05-30 02:10:50 +0000

    Added first version of test parser

--- trunk/MonoDevelop.CssEditor/Parser/Internal/CssToken.cs
+++ trunk/MonoDevelop.CssEditor/Parser/Internal/CssToken.cs
@@ -8,42 +8,50 @@
 
 namespace CssEditor.Parser.Internal {
 
-	class CssToken {
+	public enum CssTokenType
+	{
 		// Token used internally
-		public static int ERROR = 0;
+		EOF,
+		ERROR,
 	
 		// Token Definitions from the CSS Spec
-		public static int IDENT = 1;
-		public static int ATKEYWORD = 2;
-		public static int STRING = 3;
-		public static int HASH = 4;
-		public static int NUM = 5;
-		public static int PERCENTAGE = 6;
-		public static int DIMENSION = 7;
-		public static int URI = 8;
-		public static int UNICODE = 9;
-		public static int CDO = 10;
-		public static int CDC = 11;
-		public static int SEMICOLON = 12;
-		public static int LEFTCURLY = 13;
-		public static int RIGHTCURLY = 14;
-		public static int LEFTPARA = 15;
-		public static int RIGHTPARA = 16;
-		public static int LEFTSQUARE = 17;
-		public static int RIGHTSQUARE = 18;
-		public static int SPACE = 19;
-		public static int COMMENT = 20;
-		public static int FUNCTION = 21;
-		public static int INCLUDES = 22;
-		public static int DASHMATCH = 23;
-		public static int DELIM = 24;
+		IDENT,
+		ATKEYWORD,
+		STRING,
+		HASH,
+		COLON,
+		COMMA,
+		NUM,
+		PERCENTAGE,
+		DIMENSION,
+		URI,
+		UNICODE,
+		CDO,
+		CDC,
+		SEMICOLON,
+		LEFTCURLY,
+		RIGHTCURLY,
+		DOT,
+		LEFTPARA,
+		RIGHTPARA,
+		LEFTSQUARE,
+		RIGHTSQUARE,
+		SPACE,
+		COMMENT,
+		FUNCTION,
+		INCLUDES,
+		DASHMATCH,
+		DELIM
+	}
+
+	class CssToken {
 		
 		int line = 0;
 		int col = 0;
-		int type = 0;
+		CssTokenType type;
 		string value = null;
 		
-		public CssToken(string value, int type, int line, int col)
+		public CssToken(string value, CssTokenType type, int line, int col)
 		{
 			this.value = value;
 			this.type = type;
@@ -51,7 +59,7 @@ namespace CssEditor.Parser.Internal {
 			this.line = line;
 		}
 		
-		public int GetTokenType()
+		public CssTokenType GetTokenType()
 		{
 			return type;
 		}
