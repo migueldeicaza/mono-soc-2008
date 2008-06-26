d496ad4883d45512ffcad38df4d81fd328f8cf7a
nestor.salceda <nestor.salceda@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-06-26 15:30:18 +0000

    Going toward the 0 test failed.

--- gendarme/rules/Gendarme.Rules.Reliability/AvoidCallingProblematicMethodsRule.cs
+++ gendarme/rules/Gendarme.Rules.Reliability/AvoidCallingProblematicMethodsRule.cs
@@ -27,18 +27,49 @@
 //
 
 using System;
+using System.Collections.Generic;
 using Gendarme.Framework;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Gendarme.Rules.Reliability {
 	[Problem ("There are potentially dangerous calls into your code.")]
 	[Solution ("You should remove or replace the call to the dangerous method.")]
 	public class AvoidCallingProblematicMethodsRule : Rule, IMethodRule {
+		List<string> problematicMethods = new List<string> (); 
+		
+		public AvoidCallingProblematicMethodsRule ()
+		{
+			problematicMethods.Add ("System.Void System.GC::Collect()");
+			problematicMethods.Add ("System.Void System.Threading.Thread::Suspend()");
+			problematicMethods.Add ("System.Void System.Threading.Thread::Resume()");
+			problematicMethods.Add ("System.Object System.Type::InvokeMember(System.String,System.Reflection.BindingFlags,System.Reflection.Binder,System.Object,System.Object[])");
+			problematicMethods.Add ("System.IntPtr System.Runtime.InteropServices.SafeHandle::DangerousGetHandle()");
+			problematicMethods.Add ("System.Reflection.Assembly System.Reflection.Assembly::LoadFrom(System.String)");
+			problematicMethods.Add ("System.Reflection.Assembly System.Reflection.Assembly::LoadFile(System.String)");
+			problematicMethods.Add ("System.Reflection.Assembly System.Reflection.Assembly::LoadFile(System.String)");
+		}
+
+		private bool IsCallInstruction (Instruction instruction)
+		{
+			return instruction.OpCode == OpCodes.Call || 
+				instruction.OpCode == OpCodes.Callvirt;
+		}
+
+		private bool IsProblematicCall (Instruction call)
+		{
+			return problematicMethods.Contains (call.Operand.ToString ());
+		}
+
 		public RuleResult CheckMethod (MethodDefinition method)
 		{
 			if (!method.HasBody)
 				return RuleResult.DoesNotApply;
 
+			foreach (Instruction instruction in method.Body.Instructions) 
+				if (IsCallInstruction (instruction) && IsProblematicCall (instruction))
+					Runner.Report (method, instruction, Severity.Critical, Confidence.High, "You are calling a potentially dangerous method.");
+				
 			return Runner.CurrentRuleResult;
 		}
 	}
