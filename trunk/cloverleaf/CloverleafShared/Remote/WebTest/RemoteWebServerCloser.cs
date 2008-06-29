fa04498cc3c4b364c33505dcfb4ba376505bc9c5
Ed.Ropple <Ed.Ropple@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-06-29 01:07:17 +0000

    Basic XSP2 remote running support included. Multiple users won't run over each others' currently running XSP2 installs (or won't when I implement a way to see if XSP2 is already running on a given port, right now it always uses 8080 and so multiple users will choke and die), as I'm using pgrep -l. The addition of the -n parameter to pgrep seems to be sufficient to catch the last-invoked XSP2 server for that user, but there could be issues with that; I have to search for the last invocation of Mono as XSP2 isn't a separate application. The upside of this is that since I'm pulling the pid right after invoking XSP2, pgrep returning the Wrong Thing seems to be insanely rare.
    There *is* a chance of orphaned invocations if the local user (at the development machine, not the
    testing box) does something really stupid like turning off his computer without first killing the
    remote server. I don't really know how to deal with that.

---
+++ trunk/cloverleaf/CloverleafShared/Remote/WebTest/RemoteWebServerCloser.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Tamir.SharpSsh;
+
+namespace CloverleafShared.Remote.WebTest
+{
+    public partial class RemoteWebServerCloser : Form
+    {
+        String closeScriptPath;
+        String host;
+        String user;
+        String password;
+        public RemoteWebServerCloser(String scriptPath, String sshHost, String sshUser, String sshPass)
+        {
+            InitializeComponent();
+
+            closeScriptPath = scriptPath;
+            host = sshHost;
+            user = sshUser;
+            password = sshPass;
+        }
+
+        private void cmdClose_Click(object sender, EventArgs e)
+        {
+            SshExec ssh = new SshExec(host, user, password);
+            ssh.Connect();
+            ssh.RunCommand("/bin/bash /home/" + user + "/.cloverleaf/" + Path.GetFileName(closeScriptPath));
+
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
---
+++ trunk/cloverleaf/CloverleafShared/Remote/WebTest/RemoteWebTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CloverleafShared.Remote.WebTest
+{
+    public class RemoteWebTester : BaseWebTester
+    {
+        public RemoteWebTester(String projDir) : base(projDir)
+        {
+            CloverleafEnvironment.Initialize();
+        }
+
+        public void Go()
+        {
+            System.Windows.Forms.Application.Run(new RemoteWebSelector(projectDirectory));
+        }
+    }
+}
