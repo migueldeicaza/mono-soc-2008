1846d8ea8b0e66e112d7777beb8f1135182307e5
Ed.Ropple <Ed.Ropple@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-07-14 20:04:59 +0000

    CloverleafService is working. Right now it's a dumb script that just runs as a service (invoked with the Mono service handler; I haven't tested it on Linux yet because I'm not near my Linux box). Needs to be expanded to allow user naming of hosts, but I haven't done that yet.

--- trunk/cloverleaf/CloverleafService/Program.cs
+++ trunk/cloverleaf/CloverleafService/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ServiceProcess;
 using System.Text;
 
+using Mono.Zeroconf;
+
 namespace CloverleafService
 {
     static class Program
@@ -10,15 +12,28 @@ namespace CloverleafService
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
+        /// <remarks>
+        /// TODO: Need to provide a way to configure this for a given
+        /// machine (so the end user can name it, etc.). Eventually it
+        /// might be useful for cleaning up zombied xsp2/apache2 processes,
+        /// etc.
+        /// </remarks>
         static void Main()
         {
-            CloverleafEnvironment.Initialize();
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[] 
-			{ 
-				new AppService() 
-			};
-            ServiceBase.Run(ServicesToRun);
+            RegisterService service = new RegisterService();
+            service.Name = "Cloverleaf [" + Environment.MachineName + ", " +
+                    Environment.OSVersion.Platform.ToString() + "]";
+            service.RegType = "_http._tcp";
+            service.Port = 12321;
+            service.TxtRecord = new TxtRecord();
+            service.TxtRecord.Add(new TxtRecordItem("servicetype", "cloverleaf"));
+
+            service.Register();
+
+            while (true)
+            {
+                System.Threading.Thread.Sleep(0);
+            }
         }
     }
 }
