0a1a3978430b6414b9f3ee35d27941c34c4f6c8c
nestor.salceda <nestor.salceda@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-07-17 23:56:14 +0000

    Add the advances, I should only fill the empty method.

--- gendarme/reporter/FilterBySeverityAction.cs
+++ gendarme/reporter/FilterBySeverityAction.cs
@@ -32,16 +32,46 @@ using System.Xml.Linq;
 
 namespace Gendarme.Reporter {
 	public class FilterBySeverityAction : IAction {
+		static readonly string Critical = "Critical";
+		static readonly string High = "High";
+		static readonly string Medium = "Medium";
+		static readonly string Low = "Low";
+
+		public XDocument CreateDefectlessDocument (XDocument original)
+		{
+			return new XDocument (new XDeclaration ("1.0", "utf-8", "yes"),
+				new XProcessingInstruction ("xml-stylesheet","type='text/xsl' href='gendarme.xsl'"),
+				new XElement ("gendarme-output", original.Root.Attribute ("date"),
+				original.Root.Element ("files"),
+				original.Root.Element ("rules"),
+				new XElement ("results")));
+
+		}
+
+		public void AddDefectsWithSeverity (string severity, XDocument newDocument, XDocument original)
+		{
+		}
+
 		public XDocument Process (XDocument document)
 		{
 			string assemblyName = (from file in document.Root.Element ("files").Elements ()
 				select file.Attribute ("Name").Value.Split
 				(',')[0]).First ();
+			
+			XDocument critical = CreateDefectlessDocument (document);
+			XDocument high = CreateDefectlessDocument (document);
+			XDocument medium = CreateDefectlessDocument (document);
+			XDocument low = CreateDefectlessDocument (document);
 
-			new WriteToFileAction (String.Format ("{0}.Critical.xml", assemblyName)).Process (document);
-			new WriteToFileAction (String.Format ("{0}.High.xml", assemblyName)).Process (document);
-			new WriteToFileAction (String.Format ("{0}.Medium.xml", assemblyName)).Process (document);
-			new WriteToFileAction (String.Format ("{0}.Low.xml", assemblyName)).Process (document);
+			AddDefectsWithSeverity (Critical, critical, document);
+			AddDefectsWithSeverity (High, high, document);
+			AddDefectsWithSeverity (Medium, medium, document);
+			AddDefectsWithSeverity (Low, low, document);
+
+			new WriteToFileAction (String.Format ("{0}.Critical.xml", assemblyName)).Process (critical);
+			new WriteToFileAction (String.Format ("{0}.High.xml", assemblyName)).Process (high);
+			new WriteToFileAction (String.Format ("{0}.Medium.xml", assemblyName)).Process (medium);
+			new WriteToFileAction (String.Format ("{0}.Low.xml", assemblyName)).Process (low);
 
 			return document;
 		}
