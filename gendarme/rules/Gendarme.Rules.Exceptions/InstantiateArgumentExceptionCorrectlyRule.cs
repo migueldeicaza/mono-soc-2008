3adc9a075acc13d2cb47d36a69a2d713b25eb54f
nestor.salceda <nestor.salceda@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-07-24 18:14:50 +0000

    Tests and code for the first case.

--- gendarme/rules/Gendarme.Rules.Exceptions/InstantiateArgumentExceptionCorrectlyRule.cs
+++ gendarme/rules/Gendarme.Rules.Exceptions/InstantiateArgumentExceptionCorrectlyRule.cs
@@ -26,17 +26,71 @@
 // WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
+using System;
 using Gendarme.Framework;
 using Mono.Cecil;
+using Mono.Cecil.Cil;
 
 namespace Gendarme.Rules.Exceptions {
 	[Problem ("")]
 	[Solution ("")]
 	public class InstantiateArgumentExceptionCorrectlyRule : Rule, IMethodRule {
+
+		public static bool IsThrowingArgumentException (Instruction throwInstruction)
+		{
+			if (throwInstruction.Previous.OpCode == OpCodes.Newobj) {
+				Instruction instantiation = throwInstruction.Previous;
+				MethodReference method = (MethodReference) instantiation.Operand;
+				return String.Compare (method.DeclaringType.FullName, "System.ArgumentException") == 0;
+			}
+			return false;
+		}
+
+		public static bool OperandsAreInCorrectOrder (MethodDefinition method, Instruction throwInstruction)
+		{
+			int parameters = ((MethodReference) throwInstruction.Previous.Operand).Parameters.Count;
+			
+			Instruction current = throwInstruction;
+			int counter = parameters;
+			while (current != null && counter != 0) {
+				if (current.OpCode == OpCodes.Ldstr) {
+					string operand = (string) current.Operand;
+					//The second operand, a parameter name
+					//(without space)
+					if (parameters == counter) {
+						if (operand.Contains (" "))
+							return false;
+					}
+					//The first operand, would be handy to
+					//have a description
+					else {
+						if (!operand.Contains (" "))
+							return false;
+					}
+					counter--;
+				}
+				current = current.Previous;
+			}
+			
+			return true;
+		}
+
 		public RuleResult CheckMethod (MethodDefinition method)
 		{
 			if (!method.HasBody)
 				return RuleResult.DoesNotApply;
+
+			foreach (Instruction current in method.Body.Instructions) {
+				if (current.OpCode != OpCodes.Throw) 
+					continue;
+				
+				if (!IsThrowingArgumentException (current))	
+					continue;
+
+				if (!OperandsAreInCorrectOrder (method, current))
+					Runner.Report (method, current, Severity.High, Confidence.Low);
+			}
+
 			return Runner.CurrentRuleResult;
 		}
 	}
