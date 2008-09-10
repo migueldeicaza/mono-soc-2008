3949b17bbfa8be42814177768cdd1deea5dfc3d1
nestor.salceda <nestor.salceda@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-09-10 17:46:17 +0000

    Fix some of Sebastien comments.

--- trunk/gendarme/rules/Gendarme.Rules.Design/DeclareEventHandlersCorrectlyRule.cs
+++ trunk/gendarme/rules/Gendarme.Rules.Design/DeclareEventHandlersCorrectlyRule.cs
@@ -28,7 +28,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Gendarme.Framework;
 using Gendarme.Framework.Rocks;
 using Mono.Cecil;
@@ -37,78 +36,57 @@ namespace Gendarme.Rules.Design {
 	[Problem ("The delegate which handles the event haven't the correct signature.")]
 	[Solution ("You should correct the signature, return type or parameter types or parameter names.")]
 	public class DeclareEventHandlersCorrectlyRule : Rule, ITypeRule {
-		private static bool HasEventAccessors (TypeDefinition type, string fieldName)
-		{
-			MethodDefinition addEvent = type.GetMethod ("add_" + fieldName);
-			MethodDefinition removeEvent = type.GetMethod ("remove_" + fieldName);
 
-			return addEvent != null && removeEvent != null;
-		}
-
-		private static IEnumerable<TypeDefinition> GetEvents (TypeDefinition type)
-		{
-			List<TypeDefinition> result = new List<TypeDefinition> ();
-			foreach (FieldDefinition field in type.Fields) {
-				TypeDefinition fieldType = field.FieldType.Resolve ();
-				if (fieldType.IsDelegate () && HasEventAccessors (type, field.Name) && !result.Contains (fieldType))
-					result.Add (fieldType);		
-			}
-			return result;
-		}
-
-		private static MethodDefinition GetInvokeMethod (TypeDefinition type)
-		{
-			foreach (MethodDefinition method in type.Methods) {
-				if (String.Compare (method.Name, "Invoke") == 0)
-					return method;
-			}
-			return null;
-		}
-
-		private void CheckReturnVoid (TypeDefinition type, MethodDefinition invoke)
+		private void CheckReturnVoid (TypeDefinition current, MethodDefinition invoke)
 		{
 			if (String.Compare (invoke.ReturnType.ReturnType.FullName, "System.Void") != 0)
-				Runner.Report (type, Severity.Medium, Confidence.High, String.Format ("The delegate should return void, not {0}", invoke.ReturnType.ReturnType.FullName));
+				Runner.Report (current, Severity.Medium, Confidence.High, String.Format ("The delegate should return void, not {0}", invoke.ReturnType.ReturnType.FullName));
 		}
 
-		private void CheckParameterTypes (TypeDefinition type, MethodDefinition invoke)
+		private void CheckAmountOfParameters (TypeDefinition current, MethodDefinition invoke)
 		{
-			if (invoke.Parameters.Count != 2) {
-				Runner.Report (type, Severity.Low, Confidence.High, String.Format ("The signature should have 2 parameters and it has {0} parameters", invoke.Parameters.Count));
-				return;
-			}
-			
-			if (String.Compare (invoke.Parameters[0].ParameterType.FullName, "System.Object") != 0)
-				Runner.Report (type, Severity.Medium, Confidence.High, String.Format ("The first parameter should have a object, not {0}", invoke.Parameters[0].ParameterType.FullName));
-
-			if (!invoke.Parameters[1].ParameterType.Inherits ("System.EventArgs"))
-				Runner.Report (type, Severity.Medium, Confidence.High, "The second parameter should be a subclass of System.EventArgs");
+			if (invoke.Parameters.Count != 2)
+				Runner.Report (current, Severity.Medium, Confidence.High, String.Format ("The delegate should have 2 parameters"));
 		}
 
-		private void CheckParameterName (TypeDefinition type, MethodDefinition invoke, int position, string expected)
+		private void CheckParameterTypes (TypeDefinition current, MethodDefinition invoke)
+		{
+			if (invoke.Parameters.Count >= 1) {
+				if (String.Compare (invoke.Parameters[0].ParameterType.FullName, "System.Object") != 0)
+					Runner.Report (current, Severity.Medium, Confidence.High, String.Format ("The first parameter should have an object, not {0}", invoke.Parameters[0].ParameterType.FullName));
+			}
+			if (invoke.Parameters.Count >= 2) {
+				if (!invoke.Parameters[1].ParameterType.Inherits ("System.EventArgs"))
+					Runner.Report (current, Severity.Medium, Confidence.High, "The second parameter should be a subclass of System.EventArgs");
+			}
+		}
+
+		private void CheckParameterName (TypeDefinition current, MethodDefinition invoke, int position, string expected)
 		{
 			if (invoke.Parameters.Count >= position + 1) {
 				if (String.Compare (invoke.Parameters[position].Name, expected) != 0)
-					Runner.Report (type, Severity.Low, Confidence.High, String.Format ("The expected name is {0}, not {1}", expected, invoke.Parameters[position].Name));
+					Runner.Report (current, Severity.Low, Confidence.High, String.Format ("The expected name is {0}, not {1}", expected, invoke.Parameters[position].Name));
 			}
 		}
 
 		public RuleResult CheckType (TypeDefinition type)
 		{
-			IEnumerable<TypeDefinition> events = GetEvents (type);
-			if (events.Count () == 0)
+			if (type.Events.Count == 0)
 				return RuleResult.DoesNotApply;
 			
-			foreach (TypeDefinition eventType in events) {
-				MethodDefinition invoke = GetInvokeMethod (eventType);
+			foreach (EventDefinition each in type.Events) {
+				//TODO: When is with Gendarme, a invoke
+				//MethodDefinition should be created.
+				MethodDefinition invoke = each.EventType.Resolve ().GetMethod ("Invoke");
 				if (invoke != null) {
-					CheckReturnVoid (eventType, invoke);
-					CheckParameterTypes (eventType, invoke);
-					CheckParameterName (eventType, invoke, 0, "sender");
-					CheckParameterName (eventType, invoke, 1, "e");
+					CheckReturnVoid (type, invoke);
+					CheckAmountOfParameters (type, invoke);
+					CheckParameterTypes (type, invoke);
+					CheckParameterName (type, invoke, 0, "sender");
+					CheckParameterName (type, invoke, 1, "e");
 				}
 			}
-				
+							
 			return Runner.CurrentRuleResult;
 		}
 	}
