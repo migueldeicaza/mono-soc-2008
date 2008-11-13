6d27bc6871349bb27669359c2030070f3fc38a53
jeremie.laval <jeremie.laval@0a649926-8f4b-0410-bcaf-47bb3e86804f>
2008-11-13 21:31:04 +0000

    * Adding Transaction stubs

---
+++ parallelfx/System.Threading/System.Threading.Transactions/TrObject.cs
@@ -0,0 +1,51 @@
+// TrObject.cs
+//
+// Copyright (c) 2008 Jérémie "Garuma" Laval
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+//
+
+using System;
+
+namespace System.Threading.Transactions
+{
+  /* Probably better to use a interface here */
+  internal class TrObject<T> : ITrObject<T> where T : ICloneable
+  {
+	Swappable<T> obj;
+
+	// actually use TLS via Thread.SetDataSlot cause we can't go static here
+	[ThreadStatic] T copyObject;
+	[ThreadStatic] Transaction currentTransaction;
+	
+	public TrObject(T obj)
+	  {
+		this.obj = obj;
+	  }
+
+	
+
+	public void Release()
+	{
+	  
+	}
+  }
+	
+}
